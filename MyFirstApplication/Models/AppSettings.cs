0186b4903348cc640183806209c277d6347c9e53
syedomarkhalil <syedomarkhalil@hotmail.com>
2025-09-24 21:33:57 +0100

    Created seperate classes from AppSettings

--- MyFirstApplication/Models/AppSettings.cs
+++ MyFirstApplication/Models/AppSettings.cs
@@ -7,29 +7,4 @@
         public string? GoogleSignOutUrl { get; set; }
         public AuthenticationSettings? AuthenticationSettings { get; set; }
     }
-
-    public class AuthenticationSettings
-    {
-        public Facebook? Facebook { get; set; }
-        public Twitter? Twitter { get; set; }
-        public Google? Google { get; set; }
-    }
-
-    public class Twitter
-    {
-        public string? ClientId { get; set; }
-        public string? ClientSecret { get; set; }
-    }
-
-    public class Facebook
-    {
-        public string? AppId { get; set; }
-        public string? AppSecret { get; set; }
-    }
-
-    public class Google
-    {
-        public string? ClientId { get; set; }
-        public string? ClientSecret { get; set; }
-    }
 }
---
+++ MyFirstApplication/Models/Facebook.cs
@@ -0,0 +1,8 @@
+﻿namespace MyFirstApplication.Models
+{
+    public class Facebook
+    {
+        public string? AppId { get; set; }
+        public string? AppSecret { get; set; }
+    }
+}
