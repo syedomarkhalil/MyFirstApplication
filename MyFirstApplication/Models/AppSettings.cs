47834b37c93b962ddcfe193f4e7ccb2f7faaef56
syedomarkhalil <syedomarkhalil@hotmail.com>
2025-10-10 19:30:31 +0100

    Social Media Logout Refactor(facebook)

--- MyFirstApplication/Models/AppSettings.cs
+++ MyFirstApplication/Models/AppSettings.cs
@@ -5,6 +5,7 @@
         public string? BaseUri { get; set; }
         public int PageSize { get; set; }
         public string? GoogleSignOutUrl { get; set; }
+        public string? FacebookSignOutUrl { get; set; }
         public AuthenticationSettings? AuthenticationSettings { get; set; }
     }
 }
